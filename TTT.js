f590bb5ebef377251a3cca67b9bbd5f4d5964272
Mena <dayjacs03@gmail.com>
2025-08-21 19:54:29 -0500

    trying to work out logic

---
+++ TTT.js
@@ -0,0 +1,21 @@
+const gameBoard = {
+const board = ["", "", "", "", "", "", "", "", ""];
+}
+
+
+function Player(name, markerChoice){
+    return {
+        name,
+        markerChoice,
+        placeMarker(index){
+
+        }
+    }
+}
+
+function playGame(){
+
+}
+
+const player1 = Player("mena", "x");
+const player2 = Player("notMena", "o");
