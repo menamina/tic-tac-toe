e8b0843299ca8436141505f804cb585754a52535
Mena <dayjacs03@gmail.com>
2025-08-21 20:35:26 -0500

    working on only logic first

--- TTT.js
+++ TTT.js
@@ -1,5 +1,16 @@
-const gameBoard = {
-const board = ["", "", "", "", "", "", "", "", ""];
+let gameBoard = {
+    board: ["", "", "", "", "", "", "", "", ""],
+    winningCombos: [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],  
+        [2, 4, 6]
+    ],
+
 }
 
 
@@ -8,14 +19,42 @@ function Player(name, markerChoice){
         name,
         markerChoice,
         placeMarker(index){
-
-        }
+            if (gameBoard.board[index] === ""){
+                gameBoard.board[index] = this.markerChoice; 
+            } else {
+                console.log(`oops! spot already taken. whomp whomp.`);
+            };
+        },
     }
 }
 
-function playGame(){
-
+function gameLogic(player1, player2){
+    let eval1 = player1.markerChoice;
+    let eval2 = player2.markerChoice;
+    while (gameBoard.board !== ""){
+    if (eval1 === gameBoard.board.contains[winningCombos]){
+        console.log(`${eval1} wins!` )
+    } else if (eval2 === gameBoard.board.contains[winningCombos]){
+        console.log(`${eval2} wins!`)
+    } else {
+        console.log(`It's a tie`)
+    };
+}
+     
 }
 
-const player1 = Player("mena", "x");
-const player2 = Player("notMena", "o");
+
+const p1 = Player("mena", "x");
+const p2 = Player("notMena", "o");
+
+p1.placeMarker(0);
+p2.placeMarker(1);
+p1.placeMarker(2);
+p2.placeMarker(3);
+p1.placeMarker(4);
+p2.placeMarker(5);
+p1.placeMarker(6);
+p2.placeMarker(7);
+p1.placeMarker(8);
+console.log(gameBoard.board);
+console.log(gameLogic(p1, p2));
