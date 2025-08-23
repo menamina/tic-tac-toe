f2019dd80f16728d6174f890598e6b6981d998e6
Mena <dayjacs03@gmail.com>
2025-08-23 18:07:15 -0500

    working on adding the game to the dom

--- TTT.js
+++ TTT.js
@@ -13,36 +13,67 @@ let gameBoard = {
 
 }
 
-
 function Player(name, markerChoice){
+
     return {
         name,
         markerChoice,
-        placeMarker(index){
-            if (gameBoard.board[index] === ""){
-                gameBoard.board[index] = this.markerChoice; 
-            } else {
-                console.log(`oops! spot already taken. whomp whomp.`);
-            };
-        },
     }
 }
 
+
 function gameLogic(player1, player2){
-    let eval1 = player1.markerChoice;
-    let eval2 = player2.markerChoice; 
+    const displayWinner = document.querySelector(".results");
+
     function evalGame(){
-        if (gameBoard.winningCombos.some(combo => combo.every(index => gameBoard.board[index] === eval1))){
-            console.log(`${eval1} wins!`)
-        } else if (gameBoard.winningCombos.some(combo => combo.every(index => gameBoard.board[index] === eval2))){
-            console.log(`${eval2} wins!`)
+        if (gameBoard.winningCombos.some(combo => combo.every(index => gameBoard.board[index] === player1.markerChoice))){
+            console.log(`${player1.name} wins!`);
+            displayWinner.textContent = `${[player1.name]} wins!`;
+        } else if (gameBoard.winningCombos.some(combo => combo.every(index => gameBoard.board[index] === player2.markerChoice))){
+            console.log(`${player2.name} wins!`);
+            displayWinner.textContent = `${[player2.name]} wins!`;
         } else {
-            console.log(`No one wins`)
+            console.log(`No one wins`);
+            displayWinner.textContent = `Both of you are losers`;
         }
     };
     evalGame()
 }
 
+const startGame = document.querySelector(".start");
+let player1;
+let player2;
 
-const p1 = Player("mena", "x");
-const p2 = Player("notMena", "o");
+startGame.addEventListener('click', () => {
+    const playerName1 = document.querySelector(".p1Name").value;
+    const playerName2 = document.querySelector(".p2Name").value;
+    player1 = Player(playerName1, "X");
+    player2 = Player(playerName2, "O");
+    console.log(player1.name);
+    console.log(player2.name);
+
+})
+
+const displayMarkers = document.querySelector(".gameBoard");
+let playerXturn = true;
+
+displayMarkers.addEventListener('click', (e) => {
+    const putMarker = e.target;
+    const index = putMarker.dataset.index;
+    if (playerXturn && gameBoard.board[index] === ""){
+        gameBoard.board[index] = player1.marker;
+        putMarker.textContent = player1.markerChoice;
+        playerXturn = false; 
+    } else if (!playerXturn && gameBoard.board[index] === "") {
+        gameBoard.board[index] = player2.markerChoice;
+        putMarker.textContent = player2.markerChoice;
+        playerXturn = true; 
+    }
+
+    gameLogic(player1, player2);
+});
+
+const restart = document.querySelector(".restart");
+restart.addEventListener('click', () => {
+    
+})
